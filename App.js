af25725ea0c65a1427182c70e106ce3f62645cc8
Tom Workman <tworkman512@gmail.com>
2019-03-01 16:37:57 -0600

    adds a basic home screen route with RN navigation

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
 
-export default class App extends React.Component {
+class HomeScreen extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World</Text>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen. Hey, it still works!!!</Text>
       </View>
-    );
+    )
   }
 }
+
+const AppNavigator = createStackNavigator({
+  Home: HomeScreen
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends Component {
+  render() {
+    return <AppContainer />
+  }
+}
+
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Hello World. This is working!</Text>
+//       </View>
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
